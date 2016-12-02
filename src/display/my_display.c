3800817247c32c02fcc487bc9b207f70dca22038
Antonin Rapini <antonin.rapini@epitech.eu>
2016-12-02 15:31:29 +0100

    Super push script executed

--- src/display/my_display.c
+++ src/display/my_display.c
@@ -5,7 +5,7 @@
 ** Login   <antonin.rapini@epitech.net>
 ** 
 ** Started on  Wed Nov 30 16:33:23 2016 Antonin Rapini
-** Last update Fri Dec  2 14:53:39 2016 Antonin Rapini
+** Last update Fri Dec  2 15:29:32 2016 Antonin Rapini
 */
 
 #include "my_options.h"
@@ -16,7 +16,9 @@
 void		my_display(t_options *options, int i, int ac, char **av)
 {
   t_list	*files;
+  int		multiple;
 
+  multiple = ac - i > 1 ? 1 : 0;
   if (i == ac)
     {
       av[i] = malloc(sizeof(char) * 2);
@@ -28,8 +30,8 @@ void		my_display(t_options *options, int i, int ac, char **av)
     {
       if ((files = my_create_list(options, av[i])) != NULL)
 	{
-	  if (options->recursive && !options->show_self)
-	    my_putstr(my_nstrcat(3, "\n", files->fileinfos->name, ":\n"));
+	  if ((options->recursive || multiple) && !options->show_self)
+	    my_putstr(my_nstrcat(3, "\n", av[i], ":\n"));
 	  options->my_display(options, files);
 	}
       i++;
